7b83208a9ff99c8316592b88f0070739261e204e
masataka0172 <masa@DESKTOP-ODMP4DN.localdomain>
2020-03-11 14:44:56 +0900

    first commit

---
+++ hangman.py
@@ -0,0 +1,35 @@
+def hangman(word):
+    wrong=0
+    stages=["",
+            "_________   ",
+            "|           ",
+            "|     |     ",
+            "|     0     ",
+            "|   ／|＼   ",
+            "|   ／＼    ",
+            "|           ",
+            ]
+    rletters=list(word)
+    board=["_"]*len(word)
+    win=False
+    print("ハングマンへようこそ")
+    while wrong < len(stages)-1:
+        print("\n")
+        msg="1文字よそうしてね"
+        char=input(msg)
+        if char in rletters:
+            cind=rletters.index(char)
+            board[cind]='$'
+        else:
+            wrong += 1
+        print("\n".join(stages[0:e]))
+        if"_"not in board:
+            print("あなたの勝ち")
+            print(" ".join(board))
+            win=True
+            break
+    if not win:
+        print("\n".join(stages[0:wrong+1]))
+        print("あなたのマケ！正解は{}.".format(word))
+
+hangman("cat")
